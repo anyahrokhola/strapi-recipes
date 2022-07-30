42f9f509ad2a809f5f20688839a214a3a423d000
Anna <karaihr6@gmail.com>
2022-07-30 11:31:33 +0300

    fixed data

--- src/api/recipe-step/services/recipe-step.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * recipe-step service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::recipe-step.recipe-step');
